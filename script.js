4c64311c35b31e10855c108cf543ca0580ff5e67
tiaaaw2 <rthia1959@gmail.com>
2025-01-08 14:40:32 -0800

    Fix to-do list input design

--- script.js
+++ script.js
@@ -7,20 +7,20 @@ const btnEdit = document.getElementById("edit-btn");
 function add(){
     let value = document.getElementById('input').value
     let li = document.createElement('li')
+    li.id = `todo-${todo.length+1}`;
     li.innerHTML = `
-            <div id="todo-${todo.length}"> 
             <div class="todo-title">
              ${value} 
             </div>
                 <div class="todo-actions">
-                        <button id="edit-btn" class="edit-btn" data-index="0" onclick="openEditModal(${todo.length})">
+                        <button id="edit-btn" class="edit-btn" data-index="0" onclick="openEditModal(${todo.length+1})">
                             edit<i class="fas fa-edit"></i>
                         </button>
-                        <button class="delete-btn" data-index="0">
+                        <button class="delete-btn" data-index="0" onclick="openDeleteModal(${todo.length+1})">
                             delete<i class="fas fa-trash-alt"></i>
                         </button>
-                </div>
             </div>
+            
     `
     document.getElementById('list').appendChild(li)
     document.getElementById('input').value = ''
@@ -28,38 +28,21 @@ function add(){
     li.addEventListener('click', function(){
         li.classList.toggle('completed')
     })
-    todo.push(value);
 
-}
-// menampilkan terus
-function read() {
-    const todosContainer = document.querySelector(".todos-container");
-    todosContainer.innerHTML = ""; 
-    
-    
-    todo.forEach((todo, id) => {
-        const todoItem = document.createElement("div");
-        todoItem.classList.add("todo-item");
-        todoItem.innerHTML = `
-            <p>${todo}</p> <!-- Tampilkan item -->
-            <div class="todo-actions">
-                <button class="edit-btn" onclick="openEditPopup(${selectedId})">
-                    <i class="fas fa-edit"></i>
-                </button>
-                <button class="delete-btn" onclick="openDeletePopup(${selectedId})">
-                    <i class="fas fa-trash-alt"></i>
-                </button>
-            </div>
-        `;
-        todosContainer.appendChild(todoItem); 
-    });
+    createToDo = {
+        id : todo.length + 1, //unutk membrikan kode unik natara todo satu dan todo lainnya
+        title : value,
+        complete : false,
+    }
+
+    todo.push(createToDo);
 }
 
 function openEditModal(id) {
     const modal = document.getElementById("edit-modal");
     modal.style.display = "block";
     const input = document.getElementById("edit-input");
-    input.value = todo[id];
+    input.value = todo.find((v)=> v.id === id).title;
     selectedId = id;
 }
 function closeEditModal(id) {
@@ -71,9 +54,38 @@ function closeEditModal(id) {
 
 function submitEditModal() {
     let value = document.getElementById('edit-input').value
-    todo[selectedId] = value;
-    const selectedToDo = document.getElementById(`todo-${selectedId}`)
-    selectedToDo.querySelector('.todo-title').innerText = value;
+    // todo[selectedId] = value;
+
+    // untuk mengedit
+    todo = todo.map((v)=> {
+        if(v.id === selectedId){
+            return {...v, title: value}
+        }
+        return v
+    }
+
+);
+    const elementToDo = document.getElementById(`todo-${selectedId}`)
+    elementToDo.querySelector('.todo-title').innerText = value;
+    closeEditModal()
+}
+function openDeleteModal(id) {
+    const modal = document.getElementById("delete-modal");
+    modal.style.display = "block";
+    selectedId = id;
+}
+function deleteToDo() {
+    todo = todo.filter ((v)=> v.id !== selectedId)
+    //to do yg bukan mau dihpaus
+    const elementToDo = document.getElementById(`todo-${selectedId}`)
+    elementToDo.parentNode.removeChild(elementToDo);
+    closeDeleteModal()
+}
+function closeDeleteModal(id) {
+    const modal = document.getElementById("delete-modal");
+    modal.style.display = "none";
+    selectedId=null;
+
 }
 
 
